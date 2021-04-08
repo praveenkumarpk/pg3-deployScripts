a3953101bed7ea51c152ac9ccbfc2be5e64b6413
praveenkumarpk <user@DESKTOP-O6Q2J7C.localdomain>
2021-04-08 13:24:34 +0530

    flask updated

--- copy_shares.sh
+++ copy_shares.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp  $SHARED_HOME/* $APP_HOME/current -rf
+#cp  $SHARED_HOME/* $APP_HOME/current -rf
 cp  $SHARED_HOME/.env $APP_HOME/current
-cp  $SHARED_HOME/.env.staging $APP_HOME/current
+#cp  $SHARED_HOME/.env.staging $APP_HOME/current
